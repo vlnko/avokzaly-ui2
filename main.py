deb20da50f74eae78f9664c73899dc3b2d664be3
vlnko <laslolnko@gmail.com>
2025-04-16 10:42:08 +0300

    trips and success

--- main.py
+++ main.py
@@ -14,5 +14,20 @@ def passengers_page():
     return render_template("passengers.html", page_title="Passengers")
 
 
+@app.route("/success")
+def success_page():
+    return render_template("success.html", page_title="Success page")
+
+
+@app.route("/my-tickets")
+def my_tickets_page():
+    return render_template("my-tickets.html", page_title="My tickets")
+
+
+@app.route("/trips")
+def trips_page():
+    return render_template("trips.html", page_title="Trips")
+
+
 if __name__ == '__main__':
 	app.run(debug=False)
